49ac2ef235f9602c4cdd49f3de4a1433d482aef2
Gaurav Segan <gauravsegan@Gauravs-MacBook-Pro.local>
2019-10-25 01:41:26 -0500

    change to app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          pota po
+          feature branch pe change sha sha sha
         </p>
         <a
           className="App-link"
